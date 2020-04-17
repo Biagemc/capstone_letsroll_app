c7b83293ccd28d72bf4a1cf6aa90379393b06b70
biagemc <biagemc@gmail.com>
2020-04-16 21:56:15 -0400

    add CRUD actions for User model

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,54 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render "index.json.jb"
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      city: params[:city],
+      gym: params[:gym],
+      afiliation: params[:afiliation],
+      avatar: params[:avatar],
+      roll_time: params[:roll_time],
+      belt: params[:belt],
+    )
+    if @user.save
+      render "show.json.jb"
+    else
+      render json: { errors: errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.name = params[:name] || @user.name
+    @user.email = params[:email] || @user.email
+    @user.city = params[:city] || @user.city
+    @user.gym = params[:gym] || @user.gym
+    @user.afiliation = params[:afiliation] || @user.afiliation
+    @user.avatar = params[:avatar] || @user.avatar
+    @user.roll_time = params[:roll_time] || @user.roll_time
+    @user.belt = params[:belt] || @user.belt
+
+    if @user.save
+      render "show.json.jb"
+    else
+      render json: { errors: errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      render json: { message: "User successfuly destroyed..." }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,17 @@ Rails.application.routes.draw do
     post "/positions" => "positions#create"
     patch "/positions/:id" => "positions#update"
     delete "/positions/:id" => "positions#destroy"
+
+    get "/users" => "users#index"
+    get "/users/:id" => "users#show"
+    post "/users" => "users#create"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
+
+    get "/comments" => "comments#index"
+    get "/comments/:id" => "comments#show"
+    post "/comments" => "comments#create"
+    patch "/comments" => "comments#update"
+    delete "/comments" => "comments#destroy"
   end
 end
