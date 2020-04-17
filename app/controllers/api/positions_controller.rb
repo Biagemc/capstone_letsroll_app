a6098167a6dfc1e385f93a7aafab167bb805bfde
biagemc <biagemc@gmail.com>
2020-04-16 21:37:32 -0400

    add CRUD actions to Position model

--- app/controllers/api/positions_controller.rb
+++ app/controllers/api/positions_controller.rb
@@ -1,2 +1,48 @@
 class Api::PositionsController < ApplicationController
+  def index
+    @positions = Position.all
+    render "index.json.jb"
+  end
+
+  def create
+    @position = Position.new(
+      name: params[:name],
+      url: params[:url],
+      description: params[:description],
+      user_id: params[:user_id],
+      situation: params[:situation],
+    )
+    if @position.save
+      render "show.json.jb"
+    else
+      render json: { errors: errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @position = Position.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @position = Position.find(params[:id])
+    @position.name = params[:name] || @position.name
+    @position.url = params[:url] || @position.url
+    @position.description = params[:description] || @position.description
+    @position.user_id = params[:user_id] || @position.user_id
+    @position.situation = params[:situation] || @position.situation
+
+    if @position.save
+      render "show.json.jb"
+    else
+      render json: { errors: errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @position = Position.find(params[:id])
+    if @position.destroy
+      render json: { message: "Position successfuly destroyed..." }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/positions" => "positions#index"
+    get "/positions/:id" => "positions#show"
+    post "/positions" => "positions#create"
+    patch "/positions/:id" => "positions#update"
+    delete "/positions/:id" => "positions#destroy"
+  end
 end
